649e1b815a845af051f80d30402209226facc2cf
navneetyadavv <navneetyadav49710@gmail.com>
2024-09-20 19:23:38 +0530

    UI Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,18 +6,18 @@ export default function Footer() {
   return (
     <div className={styles.footerContainer}>
       <div className={styles.footerInnerContainer}>
-        <FaGithub
-          className={`${styles.footerLink} ${styles.github}`}
-        ></FaGithub>
-        <FaInstagram
-          className={`${styles.footerLink} ${styles.instagram}`}
-        ></FaInstagram>
-        <FaLinkedin
-          className={`${styles.footerLink} ${styles.linkedin}`}
-        ></FaLinkedin>
-        <FaTelegram
-          className={`${styles.footerLink} ${styles.telegram}`}
-        ></FaTelegram>
+        <a href="https://github.com/navneetyadavv">
+          <FaGithub className={`${styles.footerLink} ${styles.github}`} />
+        </a>
+        <a href="https://www.linkedin.com/in/navneet-yadav-19183423a/">
+          <FaLinkedin className={`${styles.footerLink} ${styles.linkedin}`} />
+        </a>
+        <a href="https://www.instagram.com/whyisthisnav?utm_source=qr&igsh=MTY4YTM5MDBvaWJ3bg==">
+          <FaInstagram className={`${styles.footerLink} ${styles.instagram}`} />
+        </a>
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=navneetyadav49710@gmail.com&su=Hello%20there">
+          <FaTelegram className={`${styles.footerLink} ${styles.telegram}`} />
+        </a>
       </div>
       <div className={styles.copyrights}>
         © <span>Navneet Yadav</span> . All rights reserved.
