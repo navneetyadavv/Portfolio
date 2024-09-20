15eb788e2a195a067ca6eb5ee83e680d3b43b206
navneetyadavv <navneetyadav49710@gmail.com>
2024-09-20 14:29:43 +0530

    UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
-import Starfield from "react-starfield";
+// import Starfield from "react-starfield";
 
 
 
 function App() {
   return (
     <>
-     <Starfield
+     {/* <Starfield
           starCount={1000}
           starColor={[255, 255, 255]}
           speedFactor={0.05}
           // backgroundColor="black"
-        />
+        /> */}
       <Header></Header>
       <Hero></Hero>
       <About></About>
